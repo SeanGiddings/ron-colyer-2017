9e012f4352db5efb889b7cbd874a59bc7dc4f407
Sean Giddings <seandgiddings@gmail.com>
2017-12-29 13:52:19 -0500

    created footer, added basic contact content, changed color

--- rc-2017/resources/views/partials/footer.blade.php
+++ rc-2017/resources/views/partials/footer.blade.php
@@ -1,6 +1,10 @@
-<footer class="content-info">
-  <div class="container">
-    <p>Ron Colyer</p>
+<footer class="footer__container content-info">
+  <div class="footer__contact container">
+    Colyer Contruction and Remodeling <br>
+    12345 XYZ Boulevard <br>
+    Louisville, KY 40123 <br>
+    502-123-4567
+    <div class="footer__copyright"><p>&copy;Ron Colyer 2018</p></div>
     @php(dynamic_sidebar('sidebar-footer'))
   </div>
 </footer>
