854a1987a82f9474be6aa7f28a92dcdb2612038b
Sean Giddings <seandgiddings@gmail.com>
2017-12-30 02:51:01 -0500

    created Services page, added title, construction, remodeling blades

---
+++ rc-2017/resources/views/partials/services-remodeling.blade.php
@@ -0,0 +1,11 @@
+<div class="construction__container">
+  <div class="construction__image">
+    <img src="https://secureservercdn.net/4dc.b30.godaddywp.com/wp-content/uploads/2015/02/New-Home.jpg" alt="">
+  </div>
+  <div class="construction__title">
+    Construction
+  </div>
+  <div class="construction__description">
+    Since 1981, Ron Colyer and his team of experts have designed, built and remodeled numerous award-winning homes in some of the most established neighborhoods in the Louisville Metro area. Ben Tyler Building and Remodeling consistently ranks in the top five firms in Business First's List of Full-Service Remodelers. Whether your project includes a new addition, a luxury kitchen, that dream bedroom, or a more spacious bathroom, you can rest assured that Ben Tyler will design and deliver a premium creative construction solution. The same holds true for our whole-house renovations and new home construction.
+  </div>
+</div>
---
+++ rc-2017/resources/views/services.blade.php
@@ -0,0 +1,16 @@
+{{--
+  Template Name: Services
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    @include('partials.services-title')
+    @include('partials.services-construction')
+    @include('partials.services-remodeling')
+    @include('partials.content-page')
+  @endwhile
+@endsection
