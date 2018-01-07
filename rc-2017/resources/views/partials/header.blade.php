978dd761ed7391c42e4d25fdf7a0af45a4cc9135
Sean Giddings <seandgiddings@gmail.com>
2018-01-07 15:19:58 -0500

    changed logo from temp dog image to house icon

--- rc-2017/resources/views/partials/header.blade.php
+++ rc-2017/resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
   <div class="header__container container-fluid fixed-top">
     <nav class="header__navbar navbar navbar-expand-lg navbar-light">
       <a class="header__brand navbar-brand" href="{{ home_url('/') }}">
-        <img class="header__logo d-inline-block align-top" src="https://seeklogo.com/images/D/dog-logo-0C1AEE70F1-seeklogo.com.png" alt="">
+        <img class="header__logo d-inline-block align-top" src="http://www.clker.com/cliparts/D/W/n/h/n/W/house-logo-hi.png" alt="">
         {{ get_bloginfo('name', 'display') }}
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,7 +12,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="header__nav-primary navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/services">Services</a>
