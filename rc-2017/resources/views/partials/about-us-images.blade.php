37061a6ced3ac4933c46873137361d77912fb692
Sean Giddings <seandgiddings@gmail.com>
2018-01-07 15:02:10 -0500

    Created About Us Page Blades, created relative css files, created styling, added images, and began work on the About Us Page. This was work i did while the .git file was corrupted.

---
+++ rc-2017/resources/views/partials/about-us-images.blade.php
@@ -0,0 +1,11 @@
+<div class="about-us-images__container">
+  <div class="about-us-images__left">
+    <img src="https://img1.cgtrader.com/items/9102/2fb8b1bf75/large/3d-model-luxury-detailed-house-cutaway-3d-model-max.jpg" alt="">
+  </div>
+  <div class="about-us-images__center">
+    <img src="https://img1.cgtrader.com/items/9102/2fb8b1bf75/large/3d-model-luxury-detailed-house-cutaway-3d-model-max.jpg" alt="">
+  </div>
+  <div class="about-us-images__right">
+    <img src="https://img1.cgtrader.com/items/9102/2fb8b1bf75/large/3d-model-luxury-detailed-house-cutaway-3d-model-max.jpg" alt="">
+  </div>
+</div>
