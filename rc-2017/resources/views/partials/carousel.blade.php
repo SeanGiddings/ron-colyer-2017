0e92c5025d1add0e29df6318cfd199a31379d59d
Sean Giddings <seandgiddings@gmail.com>
2018-02-04 16:20:44 -0500

    added images to files, centered text and changed font.

--- rc-2017/resources/views/partials/carousel.blade.php
+++ rc-2017/resources/views/partials/carousel.blade.php
@@ -27,13 +27,13 @@
       </div>
     </div>
     <div class="carousel-item active">
-      <img class="d-block w-100" src="http://2.bp.blogspot.com/-zZ9U7RvRmoE/U2FXV49pjiI/AAAAAAAAABo/6OTSXdDFh3o/s1600/owen+sound+house.jpg" alt="First slide">
+      <img class="d-block w-100" src="https://i.imgur.com/pkXookE.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="http://monstermathclub.com/wp-content/uploads/2017/02/nice-homes-great-nice-houses-on-the-beach.jpg" alt="Second slide">
+      <img class="d-block w-100" src="https://i.imgur.com/GjtKA1B.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://www.summithomes.com.au/remote.axd/d29m536p1h7qar.cloudfront.net/files/0R1J4U3V1R4B0Y8C2D2K3A4A9E3Q/0B194R755Q221A4A4I9M6B863Y7T/The_Danali.jpg?width=1600&height=880&anchor=center&mode=crop" alt="Third slide">
+      <img class="d-block w-100" src="https://i.imgur.com/I3m9GF0.jpg" alt="Third slide">
     </div>
   </div>
 </div>
