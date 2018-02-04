104bf1c1391128d0211a6609a14e634bad2a96d2
Sean Giddings <seandgiddings@gmail.com>
2018-02-04 16:23:04 -0500

    changed background image of contact form

--- rc-2017/resources/views/partials/footer.blade.php
+++ rc-2017/resources/views/partials/footer.blade.php
@@ -1,10 +1,26 @@
 <footer class="footer__container content-info">
-  <div class="footer__contact container">
-    Colyer Contruction and Remodeling <br>
-    12345 XYZ Boulevard <br>
-    Louisville, KY 40123 <br>
-    502-123-4567
-    <div class="footer__copyright"><p>&copy;Ron Colyer 2018</p></div>
-    @php(dynamic_sidebar('sidebar-footer'))
+  <div class="footer__contact-container">
+    <div class="footer__contact-title">
+      Contact Us
+    </div>
+    <div class="footer__contact">
+      Colyer Construction and Remodeling <br>
+      4103 Galway Cove <br>
+      Buckner, KY 40010 <br>
+      502-225-6430
+    </div>
   </div>
+  <div class="footer__icons-container">
+    <div class="footer__hba">
+      <img src="https://bialouisville.com/wp-content/themes/bialouisville/res/images/logo_bial.png" alt="">
+    </div>
+    <div class="footer__hbak">
+      <img src="http://www.hbak.com/sites/all/themes/hbak/logo.png" alt="">
+    </div>
+    <div class="footer__bia">
+      <img src="https://bialouisville.com/wp-content/uploads/2013/11/BIAGLLogo_Remodeler.png" alt="">
+    </div>
+  </div>
+  <div class="footer__sean-giddings">Web Design by Sean Giddings</div>
+    @php(dynamic_sidebar('sidebar-footer'))
 </footer>
