d522a3731e00b1ef3e1b5a534bddfb1be8602b23
Sean Giddings <seandgiddings@gmail.com>
2018-02-16 16:48:41 -0500

    added final photo to about me page, linked to proper icons in filesystem

--- rc-2017/resources/views/partials/about-us-description.blade.php
+++ rc-2017/resources/views/partials/about-us-description.blade.php
@@ -7,24 +7,24 @@
     <hr>
 
     <div class="about-us-description__description">
-      Ron Colyer has over 30 years experience in the construction industry and works directly with each client to ensure total and complete satisfaction throughout the building process.
+      Ron Colyer has over 40 years experience in the construction industry and works directly with each client to ensure total and complete satisfaction throughout the building process.
       <br><br>The moment you walk into a Colyer built custom home you feel the difference. The craftsmanship, evident throughout the home, sets it apart from other homes. Ron doesn’t just build homes, he builds relationships with his clients.
       <br><br>He is personally committed to, and involved with, every aspect of your home. From lot selection through the end of the building process, you’ll know that Ron is not only working for you… he’s working with you.
 
       <div class="about-us-description__icons">
         <div class="about-us-description__hba">
-          <img src="https://bialouisville.com/wp-content/themes/bialouisville/res/images/logo_bial.png" alt="">
+          <img src="@asset('images/icons/hba-icon.png')" alt="">
         </div>
         <div class="about-us-description__hbak">
-          <img src="http://www.hbak.com/sites/all/themes/hbak/logo.png" alt="">
+          <img src="@asset('images/icons/hbak-icon.png')" alt="">
         </div>
         <div class="about-us-description__bia">
-          <img src="https://i.imgur.com/vq8EbFq.jpg" alt="">
+          <img src="@asset('images/icons/bia-icon.png')" alt="">
         </div>
       </div>
 
-      As a registered builder of the Home Builders Association of Louisville, Ron is committed to adhering to the high standards the Association requires.
-      <br><br>As a Registered Builder, ron is required to have annual continuing education where he gains awareness of the latest construction methods and high-tech building methods. This also allows him to stay current with all building codes and regulations. He also keeps up with the design and decorating styles to offer his clients the latest trends in home building.
+      As a registered builder of the Building Industry Association of Louisville, Ron is committed to adhering to the high standards the Association requires.
+      <br><br>As a Registered Builder, Ron is required to have annual continuing education where he gains awareness of the latest construction methods and high-tech building methods. This also allows him to stay current with all building codes and regulations. He also keeps up with the design and decorating styles to offer his clients the latest trends in home building.
 
     </div>
   </div>
--- rc-2017/resources/views/partials/about-us-images.blade.php
+++ rc-2017/resources/views/partials/about-us-images.blade.php
@@ -4,7 +4,7 @@
       <img src="https://www.minutemeninc.com/images/minutemen/temp-construction-workers.jpg" alt="">
     </div>
     <div class="about-us-images__item about-us-images__item-center">
-      <img src="https://i.pinimg.com/originals/cd/07/38/cd073821185f51c0aeae3b3bf442e007.jpg" alt="">
+      <img src="https://i.imgur.com/lcpl8G8.png" alt="">
     </div>
     <div class="about-us-images__item about-us-images__item-right">
       <img src="http://claremore.org/wp-content/uploads/2017/11/construction-worker.jpg" alt="">
