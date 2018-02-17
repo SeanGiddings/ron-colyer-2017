ab16f1b80454a3edfa56dbce341cdced3af7b8da
Sean Giddings <seandgiddings@gmail.com>
2018-02-17 12:56:15 -0500

    corrected the content of the description, and streamlined the styles to match

--- rc-2017/resources/views/partials/company-description.blade.php
+++ rc-2017/resources/views/partials/company-description.blade.php
@@ -8,23 +8,21 @@
 <div class="company-description__bottom-line"><hr></div>
 
   <div class="company-description__paragraphs">
-    Building, remodeling, and maintaining your home is an investment into your future. When searching for an excellent company that offers quality remodeling in Louisville, KY, Ron Colyer Building & Remodeling is the premier choice. Through our services, we'll ensure your home increases in value and quality for you and your family.</div>
+    Choosing a builder can be a stressful task, but Colyer construction is here to alleviate that stress. Extremely professional, extraordinarily personalized, Ron Colyer is THE premier choice for construction in Louisville, KY.
    
-  <div>We are not a “track builder.” We have a variety of floor plans to use “as is” of customize them to suit your personal style. We also welcome your own set of professional building plans.</div>
-  <div>Colyer-built homes includes the extras, and whether or not you see them, you can take comfort knowing they are there:</div>
-  <li>Stick-Frame construction with rafters set 16” on center</li>
-  <li>Triple steel reinforced basement walls with 8’5” ceilings are standard in all Colyer-built homes</li>
-  <li>All unfinished basements are roughed-in for a full bath</li>
-  <li>Mar-flex waterproofing with 10-year transferable warranty</li>
-  <li>50 year warranted tongue and groove sub floors are glued, nailed, then screwed to floor joists virtually eliminating “squeaks”</li>
-  <li>Communication is key to a smooth building process; therefore all Project Milestones are updated daily</li>
+    <div>
+    <br>  We are not a “track builder.” We have a variety of floor plans to use “as is” of customize them to suit your personal style. We also welcome your own set of professional building plans.
+    </div>
+    <div>
+      <br>Colyer-built homes includes the extras, and whether or not you see them, you can take comfort knowing they are there:
+    </div>
+      <br><li>Each Colyer built home is built with the highest quality and safety in mind.</li>
+      <li>Ron Colyer is continually up-to date with the newest building standards and expectations.</li>
+      <li>Once your home is completed, you will recieve the Colyer Home Reference Manual which includes information on all the items that went into making the house your home.</li>
+      <li>Communication is key to a smooth building process; therefore all Project Milestones are updated daily</li>
 
-<div>
-  Ron’s rare commitment to old fashioned craftsmanship is evident throughout every Colyer built home. Ron builds your home as if it were his own, from the exciting design and floor plans to the quality materials and finishes.
-</div>
-
-  <div>Call us at (502)225-6430 today to schedule a consultation.</div>
-  <div>We opened our Remodeling Company in 1990.</div>
-
-  <div>We are located at 4103 Galway Cove, Buckner, KY.</div>
+    <div class="company-description__final-paragraph">
+      <br>Ron’s rare commitment to old fashioned craftsmanship is evident throughout every Colyer built home. Ron builds your home as if it were his own, from the exciting design and floor plans to the quality materials and finishes.
+    </div>
+  </div>
 </div>
