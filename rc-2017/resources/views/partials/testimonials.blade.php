bf01b8f27a0aa087e3582be552757fe31d127b4f
Sean Giddings <seandgiddings@gmail.com>
2019-02-27 01:46:33 -0500

    created production build

--- rc-2017/resources/views/partials/testimonials.blade.php
+++ rc-2017/resources/views/partials/testimonials.blade.php
@@ -7,7 +7,7 @@
       <div class="carousel-item active">
         <div class="testimonials__quote-1">
           <blockquote cite="http://">
-            "We cant thank you enough for doing such a fantastic, outstanding and wonderful job from start to finish..."<br>
+            "We can't thank you enough for doing such a fantastic, outstanding and wonderful job from start to finish..."<br>
             <small>--Mike and Christy Baker</small>
           </blockquote>
         </div>
@@ -15,7 +15,7 @@
       <div class="carousel-item">
         <div class="testimonials__quote-2">
           <blockquote cite="http://">
-            "Our endless gratitude goes out... hard work and superious craftsmanship." <br>
+            "Our endless gratitude goes out... hard work and superiour craftsmanship." <br>
             <small>--Rich and Karen Leaton</small>
           </blockquote>
         </div>
